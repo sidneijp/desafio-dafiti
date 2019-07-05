446e53937322c146b1e4fdf3a6fdd412747f22f0
Sidnei Pereira <sidi.jp@gmail.com>
2019-07-05 01:35:46 -0300

    Add setup SonarCloud e alguns testes unitários

--- src/apps/core/tests.py
+++ src/apps/core/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from .models import Shoe
+
+
+@pytest.mark.unit
+def test_unit_price_for_total_stock_price():
+    shoe = Shoe(stock=100)
+    total_stock_price = 1000
+    expected = 10
+
+    result = shoe.unit_price_for(total_stock_price)
+
+    assert expected == result
+
+
+@pytest.mark.unit
+def test_unit_price_for_total_stock_price_for_empty_stock():
+    shoe = Shoe(stock=0)
+    total_stock_price = 1000
+    expected = ValueError
+
+    with pytest.raises(expected):
+        shoe.unit_price_for(total_stock_price)
+
+
+@pytest.mark.unit
+def test_unit_price_for_total_stock_price_for_invalid_stock_value():
+    shoe = Shoe(stock='10')
+    total_stock_price = 1000
+    expected = ValueError
+
+    with pytest.raises(expected):
+        shoe.unit_price_for(total_stock_price)
